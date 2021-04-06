25164e407e81d469e3be8e8950a5468c34b885a2
mainjota <69854104+mainjota@users.noreply.github.com>
2021-04-06 18:35:45 -0300

    Add files via upload

---
+++ IPtracker.py
@@ -0,0 +1,27 @@
+# Feito por: Mainjota
+# Editado, reduzido e otimizado para Py3/Py2
+# Segue no twitter: http://twitter.com/mainjota
+# Só isso mesmo, tamo junto.
+
+import os
+import urllib.request as urllib2
+import json
+import requests
+
+while True:
+    ip=input("IP Alvo: ")
+    url = "http://ip-api.com/json/"
+    response = urllib2.urlopen(url + ip)
+    data = response.read()
+    values = json.loads(data)
+
+    print(" IP do otário: " +  values['query'])
+    print(" Cidade do otário: " + values['city'])
+    print(" Internet do otário: " + values['isp'])
+    print(" País do otário: " + values['country'])
+    print(" Estado do otário: " + values['region'])
+    print(" Fuzo do horário do otário: " + values['timezone'])
+
+
+    break
+input("Pressione alguma tecla")
